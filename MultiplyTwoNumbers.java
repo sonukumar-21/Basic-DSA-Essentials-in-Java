56c980c829b072ee6ee95544c7fbc843cd66d1d7
Sonu Kumar <sonukumar21off@gmail.com>
2025-09-14 21:42:47 +0530

    program for multiplication of two numbers in JAVA.

---
+++ MultiplyTwoNumbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class MultiplyTwoNumbers{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number:");
+        int num1 = sc.nextInt();
+        System.out.println("Enter second number:");
+        int num2 = sc.nextInt();
+        int product = num1 * num2;
+        System.out.println("The product of "+num1+" and "+num2+" is: "+product);
+
+    }
+}
